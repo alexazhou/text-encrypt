32d7724244a2e984818ef7e7216aacd4f1d71e3a
zwholdu <zwholdu@gmail.com>
2012-10-13 10:36:46 +0000

    Add script for compile .py file to exe

---
+++ TextEncrypt/setup.py
@@ -0,0 +1,31 @@
+# A simple setup script to create an executable using PyQt4. This also
+# demonstrates the method for creating a Windows executable that does not have
+# an associated console.
+#
+# PyQt4app.py is a very simple type of PyQt4 application
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+
+import sys
+
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+includes = ['re']
+excludes = []
+includefiles = ['des_X2.dll','img','imageformats']
+
+setup(
+        name = "simple_PyQt4",
+        version = "0.1",
+        description = "Sample cx_Freeze PyQt4 script",
+        executables = [Executable("TextEncrypt.py", base = base, icon = "img\\TextEncrypt.ico")],
+    options = {"build_exe": {"includes":includes,"excludes":excludes, "include_files": includefiles}},
+)
+
